d4659fdfa1deccfb864ab1c7e0b79bc8d95eed63
Luimoe05 <lmoreno00528@gmail.com>
2026-05-03 00:14:37 -0400

    fixed url bug

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://repo-ai-six.vercel.app/"],
+    allow_origins=["https://repo-ai-six.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
